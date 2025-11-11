00f8e0ae4a1cbe07f7c0b5eff9d1fea63cb52c8e
Breno Cardoso <dev.brenosspc@gmail.com>
2025-11-11 13:15:25 -0300

    Update app.py
    finish

--- app.py
+++ app.py
@@ -51,6 +51,7 @@ def update_tasks(id):
     for t in tasks:
         if t.id == id:
             task = t
+            break
     
     print(task)
     if task == None:
@@ -62,6 +63,21 @@ def update_tasks(id):
     task.completed = data['completed']
     print(task)
     return jsonify({"message": "Tarefa atalizada com sucesso"})
+
+@app.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    task = None
+    for t in tasks:
+        print(t.to_dict())
+        if t.id == id:
+            task = t
+            break
+    
+    if not task:
+        return jsonify({"message": "Nao foi possivel encontrar a atividade"}), 404
+    
+    tasks.remove(task)
+    return jsonify({"message": "Tarefa deletada com sucesso"})
 
 
 if __name__ == "__main__":
